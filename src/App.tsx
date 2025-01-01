d44359b091720bb6f68a5b47b42ace8b8ddc0ecd
吉川健史 <71423029+TakeshiYoshi@users.noreply.github.com>
2025-01-01 17:28:47 +0900

    init biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { Breadcrumbs } from '@freee_jp/vibes'
+import { Breadcrumbs } from "@freee_jp/vibes";
 
 function App() {
-  return (
-    <>
-      <div className="app">
-        <Breadcrumbs
-          links={[
-            { title: '取引', url: '/hub_pages/deals' },
-            { title: '自動で経理', url: '/wallet_txns/stream' },
-          ]}
-        />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="app">
+				<Breadcrumbs
+					links={[
+						{ title: "取引", url: "/hub_pages/deals" },
+						{ title: "自動で経理", url: "/wallet_txns/stream" },
+					]}
+				/>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
